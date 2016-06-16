446251410326ee11c8f8500220df3d3a88519b53
timmaters <timmaters@home.nl>
2016-06-16 10:11:04 +0200

    add framework to project

---
+++ application/views/demo.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Naamloos document</title>
+  <link href="css/screen.css" rel="stylesheet" />
+</head>
+
+<body>
+
+  <div class="container-left">
+    <img src="img/mobile-phone.png/">
+  </div>
+  <div class="container-top-right"></div>
+  <div class="container-bottom-right"></div>
+</body>
+</html>
