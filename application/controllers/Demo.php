f7398cb99f4ba2a0d36591107c740551e4790aad
timmaters <timmaters@home.nl>
2016-06-22 12:16:05 +0200

    database, weer

--- application/controllers/Demo.php
+++ application/controllers/Demo.php
@@ -5,13 +5,9 @@ class Demo extends CI_Controller {
 
 	public function index()
 	{
-<<<<<<< HEAD
 		$this->load->model('Db');
 		$data['records'] = $this->Db->getData();
 
 		$this->load->view('demo', $data);
-=======
-		$this->load->view('index');
->>>>>>> e2efed090d8b8af3cadacbe0b08e179ac520a22a
 	}
 }
